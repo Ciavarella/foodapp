ff6bc95d0287d8398cabceadf0a0a7b0b684fba1
Victor Ciavarella <victor.ciavarella@gmail.com>
2020-03-14 19:56:58 +0100

    update store

--- src/store.tsx
+++ src/store.tsx
@@ -1,6 +1,11 @@
-import { createStore } from 'easy-peasy'
 import model from './models'
+import { createStore } from 'easy-peasy'
+import * as restaurantService from './services/restaurantservice'
 
-const store = createStore(model)
+export interface Injections {
+  restaurantService: typeof restaurantService
+}
+
+const store = createStore(model, { injections: { restaurantService } })
 
 export default store
