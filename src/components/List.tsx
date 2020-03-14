e284c630f7941b8c0ee483405291cffb462ba7ce
Victor Ciavarella <victor.ciavarella@gmail.com>
2020-03-14 13:09:07 +0100

    add component List

---
+++ src/components/List.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ItemType from '../types';
+import ListItem from './ListItem';
+
+interface ListProps {
+  data: ItemType[]
+}
+
+const List: React.FC<ListProps> = ({ data }) => {
+  return (
+    <div className="list">
+      {data.map(item =>
+        <ListItem key={item.id} item={item} />
+      )}
+    </div>
+  )
+}
+
+export default List
