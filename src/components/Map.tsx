78bf69d89d7bcd2ccc52348b3a199a3dc6fd2844
Victor Ciavarella <victor.ciavarella@gmail.com>
2020-03-15 17:50:29 +0100

    add map to show restaurants on

---
+++ src/components/Map.tsx
@@ -0,0 +1,27 @@
+import Pins from './Pins'
+import React, { useState } from 'react'
+import ReactMapGL from 'react-map-gl'
+import { useStoreState } from '../hooks'
+
+function Map() {
+  const restaurants = useStoreState(state => state.restaurant.items)
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 59.334591,
+    longitude: 18.063240,
+    zoom: 10
+  })
+
+
+  return (
+    <ReactMapGL
+      {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_KEY}
+      onViewportChange={setViewport}
+    >
+      <Pins restaurants={restaurants} />
+    </ReactMapGL>
+  )
+}
+
+export default Map
